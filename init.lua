cb13d6623bfa37d3c57b8466aa2e0be65752440d
doodle <109024735+do0dleme@users.noreply.github.com>
2022-08-19 15:09:00 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,20 +1,19 @@
-[-- Copyright (c) 2022 doodle --]
+--       MIT License
+--      Copyright (c) 2022 doodle
 
 minetest.register_on_joinplayer(function(player)
-	local name = player:get_player_name()
     minetest.chat_send_all(minetest.colorize(#ff5500,
     "[Server]: Give a warm welcome to newcomer, " .. player:get_player_name())
-end))
+    )
+end)
 
 minetest.register_on_joinplayer(function(player)
     if minetest.check_player_privs(player, {kick=true, creative=true, fly=true}) then
     minetest.chat_send_all(minetest.colorize(#ff5500,
-    "[Server]: Give a warm welcome to staff member, "..player:get_player_name())
-    ))
+    "[Server]: Give a warm welcome to staff member, ".. player:get_player_name()))
     else
     minetest.chat_send_all((minetest.colorize(#ff5500,
-    "[Server]: Welcome back to the Wilderness, "..player:get_player_name()
+    "[Server]: Welcome back to the Wilderness, ".. player:get_player_name())
     ))
-    local player = player:get_player_name()
     end
 end)
