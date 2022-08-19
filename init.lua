b04685909239abe37e0116c2f9d92f34e77db713
doodle <109024735+do0dleme@users.noreply.github.com>
2022-08-19 15:23:00 +0800

    Just changed the priv being checked

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ minetest.register_on_newplayer(function(player)
 end)
 
 minetest.register_on_joinplayer(function(player)
-    if minetest.check_player_privs(player, {kick=true, creative=true, fly=true}) then
+    if minetest.check_player_privs(player, {kick=true, creative=true, fast=true}) then
     minetest.chat_send_all(minetest.colorize("#ffa06b",
     "[Server]: Give a warm welcome to staff member, ".. player:get_player_name()))
     else
