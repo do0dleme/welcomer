f6ae0e8fbbb00224c777300010862552039ee006
doodle <109024735+do0dleme@users.noreply.github.com>
2022-08-19 15:14:17 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@
 --      Copyright (c) 2022 doodle
 
 minetest.register_on_joinplayer(function(player)
-    minetest.chat_send_all(minetest.colorize(#ff5500,
+    minetest.chat_send_all(minetest.colorize("#ffa06b",
     "[Server]: Give a warm welcome to newcomer, " .. player:get_player_name())
     )
 end)
 
 minetest.register_on_joinplayer(function(player)
     if minetest.check_player_privs(player, {kick=true, creative=true, fly=true}) then
-    minetest.chat_send_all(minetest.colorize(#ff5500,
+    minetest.chat_send_all(minetest.colorize("#ffa06b",
     "[Server]: Give a warm welcome to staff member, ".. player:get_player_name()))
     else
-    minetest.chat_send_all((minetest.colorize(#ff5500,
+    minetest.chat_send_all((minetest.colorize("#ffa06b",
     "[Server]: Welcome back to the Wilderness, ".. player:get_player_name())
     ))
     end
