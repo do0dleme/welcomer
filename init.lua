b971af8fce58200476e0b1ad2b01ac2e5606f8d0
doodle <109024735+do0dleme@users.noreply.github.com>
2022-08-23 11:41:13 +0800

    Change to fun random messages instead

--- init.lua
+++ init.lua
@@ -1,19 +1,29 @@
 --       MIT License
 --      Copyright (c) 2022 doodle, CoolJar10
 
-minetest.register_on_newplayer(function(player)
-    minetest.chat_send_all(minetest.colorize("#ffa06b",
-    "[Server]: Give a warm welcome to newcomer, " .. player:get_player_name())
-    )
-end)
+local messages = {}
+
+messages.greeting = {
+    " just slid into the Wilderness, welcome.",
+    " , welcome to the Wilderness.",
+    " -- a wild survivor just appeared.",
+    " is now a wild pokemon.",
+}
+
+function messages.get_message(mtype)
+    if RANDOM_MESSAGES then
+        return messages[mtype][math.random(1, #messages[mtype])]
+    end
+end
 
 minetest.register_on_joinplayer(function(player)
+    local player_name = player:get_player_name()
     if minetest.check_player_privs(player, {kick=true, creative=true, fast=true}) then
     minetest.chat_send_all(minetest.colorize("#ffa06b",
-    "[Server]: Give a warm welcome to staff member, ".. player:get_player_name()))
+    "[Server]: Give a warm welcome to staff member, ".. player_name))
     else
     minetest.chat_send_all((minetest.colorize("#ffa06b",
-    "[Server]: Welcome back to the Wilderness, ".. player:get_player_name())
+    "[Server]: " .. player_name .. messages.get_message"greeting")
     ))
     end
 end)
